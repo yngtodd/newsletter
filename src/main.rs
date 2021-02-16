840645497ce13fdad97b743e3d76981d17c0d3f4
yngtodd <young.todd.mk@gmail.com>
2021-02-15 23:26:07 -0500

    Use tokio for async

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use newsletter::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    run()?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,22 @@
-use newsletter::main;
+/// Test that we can get a request and that 
+/// it has no body.
+#[actix_rt::test]
+async fn health_check_works() {
+    spawn_app();
 
-#[test]
-fn dummy_test() {
-    main()
+    let client = reqwest::Client::new();
+    
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed the execute request!");
+
+     assert!(response.status().is_success());
+     assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = newsletter::run().expect("Failed to bind address!");
+    let _ = tokio::spawn(server);
 }
