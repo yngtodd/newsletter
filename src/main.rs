57d9d437401954ad39a58ec955fc487ccb21201b
yngtodd <young.todd.mk@gmail.com>
2021-02-18 22:49:25 -0500

    Add configuration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use newsletter::startup::run;
 use std::net::TcpListener;
+use newsletter::startup::run;
+use newsletter::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to port");
-
+    let configuration = get_configuration().expect("Failed to read config!");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
