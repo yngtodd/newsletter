b78ca6174cbf66ed0c109301efd07f37d20d0527
yngtodd <young.todd.mk@gmail.com>
2021-02-18 22:57:54 -0500

    Fix formatting
    Just tidying up a couple places.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use newsletter::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let configuration = get_configuration().expect("Failed to read config!");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let config = get_configuration().expect("Failed to read config!");
+    let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,7 @@
-use crate::routes::{health_check, subscribe};
-
 use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-
+use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
