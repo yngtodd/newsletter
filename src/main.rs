c2bf3635d2d2ee1e73e4f25269c77c7d585f7698
yngtodd <young.todd.mk@gmail.com>
2021-02-21 18:46:25 -0500

    Add basic logging

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let config = get_configuration().expect("Failed to read config!");
     let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
