2f5b4f413b71034ebde229f961befb31b95d8ce1
yngtodd <young.todd.mk@gmail.com>
2021-02-21 20:57:40 -0500

    Add tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, HttpResponse};
+use tracing;
+use tracing_futures::Instrument;
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
@@ -13,6 +15,21 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id, 
+        email = %form.email, 
+        name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database."
+    );
+
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,12 +40,18 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     .map_err(|e| {
-        eprintln!("Failed to execute query: {}", e);
+        tracing::error!("Failed to execute query: {:?}", e);
         HttpResponse::InternalServerError().finish()
     })?;
+
+    tracing::info!(
+        "request_id {} - New subscriber details have been saved.",
+        request_id
+    );
 
     Ok(HttpResponse::Ok().finish())
 }
