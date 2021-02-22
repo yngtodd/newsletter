c07d62c912b39221b43fb6eb0819b00f67bd1691
yngtodd <young.todd.mk@gmail.com>
2021-02-21 21:04:30 -0500

    Setup tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use env_logger::Env;
-use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use newsletter::configuration::get_configuration;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let formatting_layer = BunyanFormattingLayer::new(
+        "newsletter".into(),
+        std::io::stdout
+    );
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let config = get_configuration().expect("Failed to read config!");
     let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
