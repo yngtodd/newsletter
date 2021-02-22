f32642986d6c8e71ab38e20c32244e9000f64b37
yngtodd <young.todd.mk@gmail.com>
2021-02-22 16:29:30 -0500

    Fix spacing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -24,18 +24,12 @@ pub async fn subscribe(
     insert_subscriber(&pool, &form)
         .await
         .map_err(|_| HttpResponse::InternalServerError().finish())?;
-    
+
     Ok(HttpResponse::Ok().finish())
 }
 
-#[tracing::instrument(
-    name = "Saving new subscriber details in database",
-    skip(form, pool)
-)]
-pub async fn insert_subscriber(
-    pool:  &PgPool,
-    form: &FormData
-) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Saving new subscriber details in database", skip(form, pool))]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
